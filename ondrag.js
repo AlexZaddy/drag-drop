e7484f09ded32d78430df7bbfa0ec0f73daafe6d
AlexZaddy <74254766+AlexZaddy@users.noreply.github.com>
2021-03-18 12:55:05 +0100

    Add files via upload

--- ondrag.js
+++ ondrag.js
@@ -1,56 +1,20 @@
+//enrgister dans une variable les element css 
 
-const base = document.querySelector('.base');
-const box = document.querySelectorAll('.case');
-
-
-
-base.addEventListener('dragstart', dragStart);
-base.addEventListener('dragend', dragEnd);
-
-
-function dragStart() {
-    this.className += ' tenu';
-
-    setTimeout(() => (this.className = 'invisible'), 0);
-}
-
-function dragEnd() {
-    this.className = 'base';
-}
-
-
-for (const vide of box) {
-
-    vide.addEventListener('dragover', dragOver);
-
-    vide.addEventListener('dragenter', dragEnter);
-
-    vide.addEventListener('dragleave', dragLeave);
-
-    vide.addEventListener('drop', dragDrop);
-
+function drag(evt) {
+    //evt.dataTransfer.setData("text", evt.target.id);
+    evt.dataTransfer.setData(Element.className, evt.target.id);
 
 }
 
-
-
-function dragOver(e) {
-    e.preventDefault()
-
-
+// passer
+function dragover(evt) {
+    evt.preventDefault();
 }
-
-function dragEnter(e) {
-    e.preventDefault();
-    this.className += ' hovered';
-}
-
-function dragLeave() {
-    this.className = 'case';
-}
-
-
-function dragDrop() {
-    this.className = 'case';
-    this.append(base);
+function drop(evt) {
+    evt.preventDefault();
+    //var image = evt.dataTransfer.getData("text");
+    var boxe  = evt.dataTransfer.getData(Element.className);
+    //evt.target.appendChild(document.getElementById(image));
+    evt.target.appendChild(document.getElementById(boxe));
+    
 }
